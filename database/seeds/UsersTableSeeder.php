23f35240d9c120cb10eb652c9c3e9e252abfec2f
tfportal <developer@fundingportal.com>
2020-03-17 22:12:03 -0400

    changes

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+
+        $user = new \App\User();
+            $user->type='admin';
+            $user->name = 'Admin User';
+            $user->email = 'admin@admin.com';
+            $user->password = \Illuminate\Support\Facades\Hash::make('testpass123');
+
+        $user->save();
+    }
+}
